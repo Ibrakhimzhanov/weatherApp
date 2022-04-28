6d1038b25bda057342e8c07636bfa4ddb46a950d
Ibrakhimzhanov <unkiborg@gmail.com>
2022-04-28 09:02:52 +0500

    Закончил первый проект

--- app.js
+++ app.js
@@ -1,44 +1,11 @@
 const userSelect = document.getElementById("check-info");
 
-const API_KEY = "47e9a6f0593acb1eb9453646a55a6d9b";
-const lat = 40.75; //latitude
-const lon = 72.3333; //longitude
-
-function createOptionElement(data) {
-    const option = document.createElement("option");
-    option.value = data.name;
-    option.innerText = data.name;
-
-    userSelect.append(option);
-}
-
-document.addEventListener("DOMContentLoaded", initApp);
-
-function initApp() {
-    Promise.all([getData()])
-        .then(val => {
-            val.forEach(res => res.id == cityListreRequired(res.id) ? console.log(res.name) : console.log("ERROR"))
-        })
-}
-
-function cityListreRequired(cityId) {
-    return cityId == 1484846 ||
-        cityId == 524894 ||
-        cityId == 2643743 ||
-        cityId == 2921044 ||
-        cityId == 2658434
-}
-
-function alertError(error) {
-    alert(error.message);
-}
-
-
 //! Async Logic 
 async function getData() {
+    const API_KEY = "47e9a6f0593acb1eb9453646a55a6d9b";
     try {
         const response = await fetch(
-            `https://api.openweathermap.org/data/2.5/weather?id=${cityListreRequired()}&lang=ru&appid=${API_KEY}`
+            `https://api.openweathermap.org/data/2.5/weather?id=524901&lang=ru&appid=${API_KEY}`
         )
         const data = await response.json()
 
@@ -47,4 +14,48 @@ async function getData() {
         alertError(error)
     }
 
+}
+
+document.addEventListener("DOMContentLoaded", initApp);
+
+
+function initApp() {
+    getData().then(data => {
+        render(data)
+    })
+}
+
+function render(data) {
+    renderCity(data);
+    renderCodeCity(data);
+    renderWeather(data);
+}
+
+function renderCity(data) {
+    const cityPlace = document.getElementById('place')
+    cityPlace.innerHTML = `Место: <span>${data.name}</span>`
+}
+
+function renderCodeCity(data) {
+    const cityPlace = document.getElementById('code')
+    cityPlace.innerHTML = `Код страны: <span>${data.sys.country}</span>`
+}
+
+function renderWeather(data) {
+    let icon = `<img src="${`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}">`
+    const cityPlace = document.getElementById('weather')
+    cityPlace.innerHTML = `Погода:${icon} <span>${data.weather[0].main}/ ${data.weather[0].description} 
+    </span>`
+}
+
+
+
+function getRequiredId(value) {
+    return value;
+}
+
+
+
+function alertError(error) {
+    alert(error.message);
 }
